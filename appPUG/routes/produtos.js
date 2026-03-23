f4e37ae82e5ae6fa2a0695ae2400573944a300a9
Isabelle Sanchez <dasilvasanchezisabelle@gmail.com>
2026-03-23 09:21:11 -0300

    Versão com nodemon adicionado

--- appPUG/routes/produtos.js
+++ appPUG/routes/produtos.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const produtos = [
-  {id: 1, nome: 'Unha Postiça', preco: 100.00},
-  {id: 2, nome: 'Unha de Gel', preco: 150.00},
+  {id: 1, nome: 'Unha de Ouro', preco: 100000.00},
+  {id: 2, nome: 'Unha de Prata', preco: 150000.00},
   {id: 3, nome: 'Unha de Acrílico', preco: 120.00},
-  {id: 4, nome: 'Unha de Diamante', preco: 190.00},
-  {id: 5, nome: 'Unha de Vibranium', preco: 190.00}
+  {id: 4, nome: 'Unha de Diamante', preco: 190000.00},
+  {id: 5, nome: 'Unha de Vibranium', preco: 2000000.00}
 ];
 
 /* GET home page. */
